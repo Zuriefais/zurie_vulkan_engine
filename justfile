63e31b27162877262ce0080d174032d8999f6262
Zuriefais <zuriefais@gmail.com>
2024-07-30 18:47:33 +0300

    Update just file and Vulkano

--- justfile
+++ justfile
@@ -1,5 +1,7 @@
 run:
-    RUST_LOG=info RUSTFLAGS="-C linker=clang -C link-arg=-fuse-ld=mold" cargo run --release
+    cargo fmt
+    cargo zigbuild --release
+    mangohud RUST_LOG=info RUSTFLAGS="-C linker=clang -C link-arg=-fuse-ld=mold" ./target/release/vulcan_engine
 
 editor:
     hyprctl dispatch exec [workspace 5] zeditor $PWD
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
+use std::sync::Arc;
+
 use log::info;
-use winit::{application::ApplicationHandler, event_loop::EventLoop};
+use vulkano::{
+    command_buffer::allocator::StandardCommandBufferAllocator,
+    device::{
+        physical::PhysicalDeviceType, Device, DeviceCreateInfo, DeviceExtensions, Queue,
+        QueueCreateInfo, QueueFlags,
+    },
+    image::{view::ImageView, Image},
+    instance::{Instance, InstanceCreateFlags, InstanceCreateInfo},
+    pipeline::graphics::viewport::Viewport,
+    render_pass::{Framebuffer, FramebufferCreateInfo, RenderPass},
+    single_pass_renderpass,
+    swapchain::{Surface, Swapchain, SwapchainCreateInfo, SwapchainPresentInfo},
+    sync::{self, GpuFuture},
+    VulkanLibrary,
+};
+use winit::{
+    application::ApplicationHandler,
+    dpi::PhysicalSize,
+    event::{ElementState, KeyEvent, WindowEvent},
+    event_loop::{ActiveEventLoop, EventLoop},
+    keyboard::{KeyCode, PhysicalKey},
+    window::Window,
+};
 
 fn main() {
     env_logger::init();
@@ -7,21 +31,262 @@ fn main() {
     info!("Creating event loop");
 
     let event_loop = EventLoop::new().unwrap();
-    let mut app = App();
+
+    let mut app = App::default();
 
     event_loop.run_app(&mut app).unwrap();
 }
 
-struct App();
+pub struct State {
+    pub surface: Arc<Surface>,
+    pub device: Arc<Device>,
+    pub queue: Arc<Queue>,
+    pub recreate_swapchain: bool,
+    pub render_pass: Arc<RenderPass>,
+    pub previous_frame_end: Option<Box<dyn GpuFuture>>,
+    pub frame_buffers: Vec<Arc<Framebuffer>>,
+    pub swapchain: Arc<Swapchain>,
+    pub viewport: Viewport,
+}
+
+impl State {
+    pub async fn new(window: Arc<Window>) -> State {
+        let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
+        let required_extensions = Surface::required_extensions(&window);
+        let instance = Instance::new(
+            library,
+            InstanceCreateInfo {
+                // Enable enumerating devices that use non-conformant Vulkan implementations.
+                // (e.g. MoltenVK)
+                flags: InstanceCreateFlags::ENUMERATE_PORTABILITY,
+                enabled_extensions: required_extensions,
+                ..Default::default()
+            },
+        )
+        .expect("failed to create instance");
+        let device_extensions = DeviceExtensions {
+            khr_swapchain: true,
+            ..DeviceExtensions::empty()
+        };
+        let surface = Surface::from_window(instance.clone(), window).unwrap();
+        let (physical_device, queue_family_index) = instance
+            .enumerate_physical_devices()
+            .unwrap()
+            .filter(|p| p.supported_extensions().contains(&device_extensions))
+            .filter_map(|p| {
+                p.queue_family_properties()
+                    .iter()
+                    .enumerate()
+                    .position(|(i, q)| {
+                        // pick first queue_familiy_index that handles graphics and can draw on the surface created by winit
+                        q.queue_flags.contains(QueueFlags::GRAPHICS)
+                            && p.surface_support(i as u32, &surface).unwrap_or(false)
+                    })
+                    .map(|i| (p, i as u32))
+            })
+            .min_by_key(|(p, _)| {
+                // lower score for preferred device types
+                match p.properties().device_type {
+                    PhysicalDeviceType::DiscreteGpu => 0,
+                    PhysicalDeviceType::IntegratedGpu => 1,
+                    PhysicalDeviceType::VirtualGpu => 2,
+                    PhysicalDeviceType::Cpu => 3,
+                    PhysicalDeviceType::Other => 4,
+                    _ => 5,
+                }
+            })
+            .expect("No suitable physical device found")
+            .clone();
+        let (device, mut queues) = Device::new(
+            physical_device,
+            DeviceCreateInfo {
+                // here we pass the desired queue family to use by index
+                queue_create_infos: vec![QueueCreateInfo {
+                    queue_family_index,
+                    ..Default::default()
+                }],
+                ..Default::default()
+            },
+        )
+        .expect("failed to create device");
+        let queue = queues.next().unwrap();
+
+        let (swapchain, images) = {
+            let caps = device
+                .physical_device()
+                .surface_capabilities(&surface, Default::default())
+                .unwrap();
+
+            let usage = caps.supported_usage_flags;
+            let alpha = caps.supported_composite_alpha.into_iter().next().unwrap();
+
+            let image_format = device
+                .physical_device()
+                .surface_formats(&surface, Default::default())
+                .unwrap()[0]
+                .0;
+
+            let window = surface.object().unwrap().downcast_ref::<Window>().unwrap();
+            let image_extent: [u32; 2] = window.inner_size().into();
+
+            Swapchain::new(
+                device.clone(),
+                surface.clone(),
+                SwapchainCreateInfo {
+                    min_image_count: caps.min_image_count,
+                    image_format,
+                    image_extent,
+                    image_usage: usage,
+                    composite_alpha: alpha,
+                    ..Default::default()
+                },
+            )
+            .unwrap()
+        };
+        let command_buffer_allocator =
+            StandardCommandBufferAllocator::new(device.clone(), Default::default());
+        let render_pass = single_pass_renderpass!(
+            device.clone(),
+            attachments: {
+                // `foo` is a custom name we give to the first and only attachment.
+                color: {
+                    format: swapchain.image_format(),
+                    samples: 1,
+                    load_op: Clear,
+                    store_op: Store,
+                },
+            },
+            pass: {
+                color: [color],       // Repeat the attachment name here.
+                depth_stencil: {},
+            },
+        )
+        .unwrap();
+        let mut viewport = Viewport {
+            offset: [0.0, 0.0],
+            extent: [0.0, 0.0],
+            depth_range: (0.0..=1.0),
+        };
+
+        let frame_buffers =
+            window_size_dependent_setup(&images, render_pass.clone(), &mut viewport);
+        let previous_frame_end = Some(Box::new(sync::now(device.clone())) as Box<dyn GpuFuture>);
+        Self {
+            surface,
+            device,
+            queue,
+            recreate_swapchain: false,
+            render_pass,
+            previous_frame_end,
+            frame_buffers,
+            swapchain,
+            viewport,
+        }
+    }
+
+    pub fn render(&mut self) {
+        if self.recreate_swapchain {
+            let window = self
+                .surface
+                .object()
+                .unwrap()
+                .downcast_ref::<Window>()
+                .unwrap();
+            let image_extent: [u32; 2] = window.inner_size().into();
+
+            let (new_swapchain, new_images) = match self.swapchain.recreate(SwapchainCreateInfo {
+                image_extent,
+                ..self.swapchain.create_info()
+            }) {
+                Ok(r) => r,
+                Err(e) => panic!("Failed to recreate swapchain: {:?}", e),
+            };
+
+            self.swapchain = new_swapchain;
+            self.frame_buffers = window_size_dependent_setup(
+                &new_images,
+                self.render_pass.clone(),
+                &mut self.viewport,
+            );
+            self.recreate_swapchain = false;
+        }
+    }
+
+    pub fn resize(&mut self, size: PhysicalSize<u32>) {}
+}
+
+fn window_size_dependent_setup(
+    images: &[Arc<Image>],
+    render_pass: Arc<RenderPass>,
+    viewport: &mut Viewport,
+) -> Vec<Arc<Framebuffer>> {
+    let dimensions = images[0].extent();
+    viewport.extent = [dimensions[0] as f32, dimensions[1] as f32];
+
+    images
+        .iter()
+        .map(|image| {
+            let view = ImageView::new_default(image.clone()).unwrap();
+            Framebuffer::new(
+                render_pass.clone(),
+                FramebufferCreateInfo {
+                    attachments: vec![view],
+                    ..Default::default()
+                },
+            )
+            .unwrap()
+        })
+        .collect::<Vec<_>>()
+}
+
+#[derive(Default)]
+struct App {
+    window: Option<Arc<Window>>,
+    state: Option<State>,
+}
 
 impl ApplicationHandler for App {
-    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {}
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        info!("Creating window");
+        if self.window.is_none() {
+            let window = Arc::new(
+                event_loop
+                    .create_window(Window::default_attributes())
+                    .unwrap(),
+            );
+            self.window = Some(window.clone());
+
+            let state = pollster::block_on(State::new(window.clone()));
+            self.state = Some(state);
+        }
+    }
 
     fn window_event(
         &mut self,
         event_loop: &winit::event_loop::ActiveEventLoop,
-        window_id: winit::window::WindowId,
+        _: winit::window::WindowId,
         event: winit::event::WindowEvent,
     ) {
+        match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        state: ElementState::Pressed,
+                        physical_key: PhysicalKey::Code(KeyCode::Escape),
+                        ..
+                    },
+                ..
+            } => event_loop.exit(),
+            WindowEvent::Resized(size) => self.state.as_mut().unwrap().resize(size),
+            WindowEvent::RedrawRequested => {
+                info!("redraw requested");
+                self.state.as_mut().unwrap().render();
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => {}
+        }
     }
 }
+
+const CLEAR_VALUE: [f32; 4] = [0.0, 0.68, 1.0, 1.0];
