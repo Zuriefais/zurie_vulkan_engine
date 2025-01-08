540cd942dda6396f7806706eadc287f4b5c76473
Zuriefais <zuriefais@gmail.com>
2025-01-08 13:15:27 +0300

    Thread safe Audio manager

--- crates/zurie_audio/src/lib.rs
+++ crates/zurie_audio/src/lib.rs
@@ -1,45 +1,91 @@
 use kira::backend::cpal::CpalBackend;
+use kira::sound::static_sound::StaticSoundData;
 use kira::AudioManagerSettings;
 use kira::DefaultBackend;
-use kira::{sound::static_sound::StaticSoundData, AudioManager};
+use log::info;
 use log::warn;
 use slotmap::{new_key_type, KeyData, SlotMap};
-use std::io::Cursor;
-use std::path::Path;
 
-pub struct EngineAudioManager {
-    kira_manager: AudioManager,
-    sound_storage: SlotMap<SoundHandle, StaticSoundData>,
+use std::str::MatchIndices;
+use std::sync::mpsc::channel;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::Sender;
+
+use std::path::Path;
+use std::sync::{Arc, RwLock};
+use std::thread;
+
+#[derive(Debug)]
+pub enum AudioCommand {
+    Play(SoundHandle),
+    Load(Box<Path>, Sender<SoundHandle>),
+    Stop,
+}
+
+pub struct AudioManager {
+    manager: Sender<AudioCommand>,
+}
+
+impl AudioManager {
+    pub fn new() -> AudioManager {
+        let (sender, receiver) = channel();
+        thread::spawn(move || {
+            let mut audio_thread = AudioThread::new();
+            audio_thread.run(receiver);
+        });
+        AudioManager { manager: sender }
+    }
+
+    pub fn load_sound(&self, path: Box<Path>) -> SoundHandle {
+        let (sender, receiver) = channel();
+        self.manager.send(AudioCommand::Load(path, sender));
+        receiver.recv().unwrap()
+    }
+
+    pub fn play(&self, sound: SoundHandle) {
+        self.manager.send(AudioCommand::Play(sound)).unwrap();
+    }
 }
 
 new_key_type! { pub struct SoundHandle; }
 
-impl EngineAudioManager {
+pub struct AudioThread {
+    kira_manager: kira::AudioManager,
+    sound_storage: SlotMap<SoundHandle, StaticSoundData>,
+}
+
+impl AudioThread {
     pub fn new() -> Self {
-        EngineAudioManager {
-            kira_manager: AudioManager::<DefaultBackend>::new(AudioManagerSettings::default())
-                .unwrap(),
+        AudioThread {
+            kira_manager:
+                kira::AudioManager::<DefaultBackend>::new(AudioManagerSettings::default()).unwrap(),
             sound_storage: Default::default(),
         }
     }
 
-    pub fn load_sound(&mut self, load_info: SoundLoadInfo) -> anyhow::Result<SoundHandle> {
-        let sound_data = match load_info {
-            SoundLoadInfo::File(path) => StaticSoundData::from_file(path),
-        }?;
-        let handle = self.sound_storage.insert(sound_data);
-        Ok(handle)
+    fn run(&mut self, receiver: Receiver<AudioCommand>) {
+        while let Ok(command) = receiver.recv() {
+            info!("command received, {:?}", command);
+            match command {
+                AudioCommand::Play(sound_handle) => self.play(sound_handle),
+                AudioCommand::Load(path, sender) => self.load_sound(path, sender),
+                AudioCommand::Stop => break,
+            }
+        }
     }
 
-    pub fn play(&mut self, sound: SoundHandle) {
+    fn load_sound(&mut self, path: Box<Path>, sender: Sender<SoundHandle>) {
+        self.sound_storage.insert_with_key(|key| {
+            sender.send(key);
+            StaticSoundData::from_file(path).expect("error loading file")
+        });
+    }
+
+    fn play(&mut self, sound: SoundHandle) {
         if let Some(sound) = self.sound_storage.get(sound) {
             self.kira_manager.play(sound.clone());
         } else {
             warn!("Could't play sound from handle")
         }
     }
-}
-
-pub enum SoundLoadInfo {
-    File(Box<Path>),
 }
--- crates/zurie_audio/src/main.rs
+++ crates/zurie_audio/src/main.rs
@@ -1,16 +1,9 @@
 use std::{error::Error, io::stdin, path::Path};
-
-use kira::{
-    backend::DefaultBackend, sound::static_sound::StaticSoundData, AudioManager,
-    AudioManagerSettings,
-};
-use zurie_audio::EngineAudioManager;
+use zurie_audio::AudioManager;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut manager = EngineAudioManager::new();
-    let sound = manager.load_sound(zurie_audio::SoundLoadInfo::File(
-        Path::new("static/sound.wav").into(),
-    ))?;
+    let mut manager = AudioManager::new();
+    let sound = manager.load_sound(Path::new("static/sound.wav").into());
     loop {
         wait_for_enter_press()?;
         manager.play(sound);
